43a202b435008f54a4df532495a49817ac149ad5
d-khuu <dannykhuu322@gmail.com>
2024-05-01 08:07:21 +1000

    lcd code is mostly functional aside from debug messages

--- Core/Inc/lcd_driver.h
+++ Core/Inc/lcd_driver.h
@@ -3,22 +3,142 @@
  *
  *  Created on: Mar 31, 2024
  *      Author: Danny
+ *
+ *      Device: LCD1602 16x2 LCD I2C Module
+ *      I have no intention to include every feature of this LCD, I just want the
+ *      basic functionality of this LCD screen.
+ *
+ *      In terms of operation times, all instructions take around 40 us, except for the
+ *      return home and clear display operations which use 1.53 ms.
  */
 
 #ifndef INC_LCD_DRIVER_H_
 #define INC_LCD_DRIVER_H_
 
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-#define LCD_SLAVE_ADDRESS 0b0111110
+#define LCD_SLAVE_ADDRESS 0x3E << 1
 
-int lcd_setup();
+#define LCD_MAX_CHAR_LENGTH 32
 
-int lcd_write_string(char* output_string);
+// I2C Register Addresses
+// Main Address
+#define LCD_CONTROL_BYTE	0x00
+// CGRAM Address
+#define LCD_CGRAM_ADDRESS	0x40
+// DDRAM Address
+#define LCD_DDRAM_ADDRESS	0x80
 
-int lcd_reset();
+
+// Data Byte/Command
+// Clears the display and returns the cursor to the beginning
+#define LCD_CLEAR_DISPLAY 	0x01
+// Return Cursor to the beginning
+#define LCD_RETURN_HOME		0x02
+// Entry Mode Set
+#define	LCD_ENTRY_MODE_SET	0x04
+// Display On/Off Control
+#define LCD_DISPLAY_ONOFF_CONTROL	0x08
+// Cursor or Display Shift
+#define LCD_CURSOR_DISPLAY_SHIFT	0x10
+// Function Set
+#define	LCD_FUNCTION_SET	0x20
 
 
+// Modifiers for Data Byte/Command
 
+// Entry Mode Set Modifiers
+// Increment DDRAM Address
+#define LCD_INC_DDRAM	0x02
+// Decrement DDRAM Address
+#define LCD_DEC_DDRAM	0x00
+// Shift Entire Display
+#define	LCD_SHIFT_DISP	0x01
+// Not Shift Entire Display
+#define LCD_NO_SHIFT_DISP	0x00
+
+// Display On/Off Control Modifier
+// Display On Control Bit
+#define LCD_DISP_CTRL_ON	0x04
+// Display Off Control Bit
+#define	LCD_DISP_CTRL_OFF	0x00
+// Cursor On Control Bit
+#define LCD_CURSOR_ON	0x02
+// Cursor Off Control Bit
+#define LCD_CURSOR_OFF	0x00
+// Cursor Blink On Control Bit
+#define LCD_CURSOR_BLINK_ON	0x01
+// Cursor Blink Off Control Bit
+#define LCD_CURSOR_BLINK_OFF	0x00
+
+// Cursor or Display Shift Modifiers
+// Shift cursor to the left, AC is decreased by 1
+#define LCD_CURSOR_LEFT_DECREASE	0x00
+// Shift cursor to the right, AC is increased by 1
+#define LCD_CURSOR_RIGHT_INCREASE	0x04
+// Shift all the display to the left, cursor moves according to the display
+#define LCD_CURSOR_WITH_DISPLAY_LEFT	0x08
+// Shift all the display to the right, cursor moves according to the display
+#define LCD_CURSOR_WITH_DISPLAY_RIGHT	0x012
+
+// Function Set
+// 8-bit Bus Mode with MPU
+#define LCD_DL_8BIT	0x10
+// 4-bit Bus Mode with MPU
+#define LCD_DL_4BIT	0x00
+// 2-line Display Mode
+#define	LCD_2_LINE	0x08
+// 1-line Display Mode
+#define	LCD_1_LINE	0x00
+// 11 Dot Format Display Format
+#define LCD_11_DOT	0x04
+// 8 Dot Format Display Format
+#define LCD_8_DOT	0x00
+
+// DDRAM Miscellaneous Bits
+#define LCD_ROW_1_FIRST_ADDRESS	0x00
+#define LCD_ROW_2_FIRST_ADDRESS 0x40
+
+// Struct Definitions
+struct lcd_current_state
+{
+	int cursor_row;
+	int cursor_col;
+
+	char chars_on_screen[LCD_MAX_CHAR_LENGTH];
+
+};
+
+// Function Definitions
+
+int LCD_Setup(I2C_HandleTypeDef* i2c_handler);
+
+int LCD_Reset(I2C_HandleTypeDef* i2c_handler);
+
+int LCD_Write_Char(I2C_HandleTypeDef* i2c_handler, char output_char);
+
+int LCD_Write_String(I2C_HandleTypeDef* i2c_handler, char* output_string);
+
+int LCD_Set_Cursor_Position(I2C_HandleTypeDef* i2c_handler, int row, int col);
+
+int LCD_Return_Home(I2C_HandleTypeDef* i2c_handler);
+
+int LCD_Redraw_Current_State(I2C_HandleTypeDef* i2c_handler);
+
+int LCD_Show_Debug_Message(I2C_HandleTypeDef* i2c_handler, char* output_string);
+
+// Wrapper functions for LCD Debugging Messages
+// and returning the text back to the original state
+int LCD_Write_String_Non_Debug(I2C_HandleTypeDef* i2c_handler, char* output_string);
+
+int LCD_Set_Cursor_Position_Non_Debug(I2C_HandleTypeDef* i2c_handler, int row, int col);
+
+int LCD_Initialise_State_Struct(I2C_HandleTypeDef* i2c_handler);
+int __LCD_State_Update_Cursor_Position(int row, int col, int update_absolute_position);
+int __LCD_State_Update_LCD_Screen(I2C_HandleTypeDef* i2c_handler, char output_char);
 
 
 #endif /* INC_LCD_DRIVER_H_ */
--- Core/Src/lcd_driver.c
+++ Core/Src/lcd_driver.c
@@ -7,3 +7,228 @@
 
 #include <lcd_driver.h>
 
+struct lcd_current_state lcd_current_state_g;
+
+int LCD_Setup(I2C_HandleTypeDef* i2c_handler)
+{
+	HAL_StatusTypeDef ret;
+	uint8_t buffer[12];
+	buffer[0] = LCD_CONTROL_BYTE;
+
+	// 15ms initialisation delay
+	HAL_Delay(15);
+
+	// Set function set
+	// 2-line mode, 8-bit Bus Mode, display on
+	buffer[1] = LCD_FUNCTION_SET | LCD_DL_8BIT | LCD_2_LINE;
+	ret = HAL_I2C_Master_Transmit(i2c_handler, LCD_SLAVE_ADDRESS, buffer, 2, HAL_MAX_DELAY);
+
+	// Delay of at least 39 us
+	HAL_Delay(1);
+
+	// Set display mode
+	// display on, cursor on, cursor blink on
+	buffer[1] = LCD_DISPLAY_ONOFF_CONTROL | LCD_DISP_CTRL_ON | LCD_CURSOR_ON | LCD_CURSOR_BLINK_ON;
+	ret = HAL_I2C_Master_Transmit(i2c_handler, LCD_SLAVE_ADDRESS, buffer, 2, HAL_MAX_DELAY);
+
+	// Delay of at least 39 us
+	HAL_Delay(1);
+
+	// Clear display
+	buffer[1] = LCD_CLEAR_DISPLAY;
+	ret = HAL_I2C_Master_Transmit(i2c_handler, LCD_SLAVE_ADDRESS, buffer, 2, HAL_MAX_DELAY);
+
+
+	// Delay of at least 1.53 ms
+	HAL_Delay(2);
+
+	// Entry Mode Set
+	// Cursor moves to the right, no shift (no idea what this is)
+	buffer[1] = LCD_ENTRY_MODE_SET | LCD_INC_DDRAM | LCD_NO_SHIFT_DISP;
+	ret = HAL_I2C_Master_Transmit(i2c_handler, LCD_SLAVE_ADDRESS, buffer, 2, HAL_MAX_DELAY);
+
+	LCD_Initialise_State_Struct(i2c_handler);
+
+	return ret;
+}
+
+
+int LCD_Reset(I2C_HandleTypeDef* i2c_handler)
+{
+	HAL_StatusTypeDef ret;
+
+	uint8_t buffer[12];
+	buffer[0] = LCD_CONTROL_BYTE;
+	buffer[1] = LCD_CLEAR_DISPLAY;
+	ret = HAL_I2C_Master_Transmit(i2c_handler, LCD_SLAVE_ADDRESS, buffer, 2, HAL_MAX_DELAY);
+
+
+	// Delay of at least 1.53 ms
+	HAL_Delay(2);
+
+	return ret;
+}
+
+int LCD_Write_Char(I2C_HandleTypeDef* i2c_handler, char output_char)
+{
+	HAL_StatusTypeDef ret;
+
+	uint8_t buffer[12];
+	buffer[0] = LCD_CGRAM_ADDRESS;
+	buffer[1] = output_char;
+	ret = HAL_I2C_Master_Transmit(i2c_handler, LCD_SLAVE_ADDRESS, buffer, 2, HAL_MAX_DELAY);
+
+	return ret;
+}
+
+
+int LCD_Write_String(I2C_HandleTypeDef* i2c_handler, char* output_string)
+{
+	HAL_StatusTypeDef ret;
+	int string_count = strlen(output_string);
+
+	for(int i = 0; i<string_count; i++)
+	{
+		if(output_string[i] != '\0') ret = LCD_Write_Char(i2c_handler, output_string[i]);
+	}
+
+	return ret;
+}
+
+
+int LCD_Set_Cursor_Position(I2C_HandleTypeDef* i2c_handler, int row, int col)
+{
+	HAL_StatusTypeDef ret;
+
+	uint8_t buffer[12];
+	buffer[0] = LCD_DDRAM_ADDRESS;
+	if(row == 1)
+	{
+		buffer[1] = LCD_DDRAM_ADDRESS | LCD_ROW_1_FIRST_ADDRESS | col;
+	}
+	else if(row == 2)
+	{
+		buffer[1] = LCD_DDRAM_ADDRESS | LCD_ROW_2_FIRST_ADDRESS | col;
+	}
+	else
+	{
+		return HAL_ERROR;
+	}
+
+	ret = HAL_I2C_Master_Transmit(i2c_handler, LCD_SLAVE_ADDRESS, buffer, 2, HAL_MAX_DELAY);
+
+	return ret;
+
+}
+
+int LCD_Return_Home(I2C_HandleTypeDef* i2c_handler)
+{
+	HAL_StatusTypeDef ret;
+
+	uint8_t buffer[12];
+	buffer[0] = LCD_CONTROL_BYTE;
+	buffer[1] = LCD_RETURN_HOME;
+	ret = HAL_I2C_Master_Transmit(i2c_handler, LCD_SLAVE_ADDRESS, buffer, 2, HAL_MAX_DELAY);
+
+
+	// Delay of at least 1.53 ms
+	HAL_Delay(2);
+
+	return ret;
+}
+
+int LCD_Initialise_State_Struct(I2C_HandleTypeDef* i2c_handler)
+{
+	lcd_current_state_g.cursor_col = 0;
+	lcd_current_state_g.cursor_row = 1;
+	memset(lcd_current_state_g.chars_on_screen, 0, sizeof(lcd_current_state_g.chars_on_screen));
+//	memset(lcd_current_state_g.chars_on_screen, 0, sizeof(LCD_MAX_CHAR_LENGTH * lcd_current_state_g.chars_on_screen));
+
+	return 1;
+}
+
+
+int LCD_Redraw_Current_State(I2C_HandleTypeDef* i2c_handler)
+{
+	LCD_Reset(i2c_handler);
+	LCD_Write_String(i2c_handler, lcd_current_state_g.chars_on_screen);
+	LCD_Set_Cursor_Position(i2c_handler, lcd_current_state_g.cursor_row, lcd_current_state_g.cursor_col);
+
+	return 1;
+}
+
+
+int LCD_Show_Debug_Message(I2C_HandleTypeDef* i2c_handler, char* output_string)
+{
+	LCD_Reset(i2c_handler);
+	LCD_Write_String(i2c_handler, output_string);
+	HAL_Delay(4000);
+	LCD_Redraw_Current_State(i2c_handler);
+	return 1;
+}
+
+int LCD_Write_String_Non_Debug(I2C_HandleTypeDef* i2c_handler, char* output_string)
+{
+	//Update the internal current state struct
+	int string_count = strlen(output_string);
+
+	for(int i = 0; i<string_count; i++)
+	{
+		if(output_string[i] != '\0')
+		{
+			__LCD_State_Update_Cursor_Position(0,0,0);
+			__LCD_State_Update_LCD_Screen(i2c_handler, output_string[i]);
+		}
+	}
+	LCD_Write_String(i2c_handler, output_string);
+	return 1;
+}
+
+int LCD_Set_Cursor_Position_Non_Debug(I2C_HandleTypeDef* i2c_handler, int row, int col)
+{
+	//Update the internal current state struct
+	__LCD_State_Update_Cursor_Position(row, col, 1);
+
+
+	LCD_Set_Cursor_Position(i2c_handler, row, col);
+
+	return 1;
+}
+
+int __LCD_State_Update_Cursor_Position(int row, int col, int update_absolute_position)
+{
+	// If update to an absolute position?
+	if(update_absolute_position == 1)
+	{
+		lcd_current_state_g.cursor_row = row;
+		lcd_current_state_g.cursor_col = col;
+	}
+	// If it overflows to the second row
+	else if(lcd_current_state_g.cursor_row == 1 && lcd_current_state_g.cursor_col == 15)
+	{
+		lcd_current_state_g.cursor_row = 2;
+		lcd_current_state_g.cursor_col = 0;
+	}
+	// The LCD screen's cursor can overflow out of the screen, so no need to account for that
+	else
+	{
+		lcd_current_state_g.cursor_col++;
+	}
+
+	return 1;
+}
+
+int __LCD_State_Update_LCD_Screen(I2C_HandleTypeDef* i2c_handler, char output_char)
+{
+	int index = (lcd_current_state_g.cursor_row-1)*16 + lcd_current_state_g.cursor_col;
+
+	char * debug_message = malloc(LCD_MAX_CHAR_LENGTH * sizeof(char));
+	if(index >= LCD_MAX_CHAR_LENGTH)
+	{
+		sprintf(debug_message, "The index exceeded for %c",output_char);
+		LCD_Show_Debug_Message(i2c_handler, debug_message);
+	}
+	lcd_current_state_g.chars_on_screen[index] = output_char;
+
+	return 1;
+}
